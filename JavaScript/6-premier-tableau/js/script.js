f569aee0a5f9d0b621ce1d74b5fc099af58dee68
Julien Noyer <j.noyer@digitalworkshop.fr>
2015-11-07 17:49:40 +0100

    Create script.js

---
+++ JavaScript/6-premier-tableau/js/script.js
@@ -0,0 +1,14 @@
+var leNom = "Julien Noyer";
+
+var monTableau = ["Du texte", 2391, false, leNom];
+  
+
+// Afficher les éléments du tableau
+console.log(monTableau[0]);         // Affiche “Du texte”    
+console.log(monTableau[1]);         // Affiche 2391
+console.log(monTableau[2]);         // Affiche false
+console.log(monTableau[3]);         // Affiche “Julien Noyer”  
+
+
+// Connaître la taille d’un tableau
+console.log(monTableau.length);     // Affiche 4
