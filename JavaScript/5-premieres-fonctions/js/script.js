787e9086a08d6c0b7ced7b62b6395b8c690f4e97
Julien Noyer <j.noyer@digitalworkshop.fr>
2015-11-07 17:44:35 +0100

    Create script.js

---
+++ JavaScript/5-premieres-fonctions/js/script.js
@@ -0,0 +1,16 @@
+// Fonction simple
+function helloWorld() {
+  console.log("Hello World!");
+}
+
+helloWorld();	
+// Affiche la string “Hello World!”
+
+
+// Fonction avec paramètres
+function fullName(firstName, lastName) {
+  console.log("Hello World " + firstName + " " + lastName);
+}
+
+fullName("Julien", "Noyer");
+// Affiche la string “Hello World Julien Noyer”
