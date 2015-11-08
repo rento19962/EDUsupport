e502bc5d278d7baf83aae13abe9e288a67a1abcd
Julien Noyer <j.noyer@digitalworkshop.fr>
2015-11-08 13:55:00 +0100

    Create script.js

---
+++ JavaScript/8-premier-objet/js/script.js
@@ -0,0 +1,13 @@
+var julien = {
+  firstName: "Julien",
+  lastName: "Noyer",
+  pays: "France",
+  age: 36
+} 
+
+console.log(julien.firstName);  // Affiche "Julien"
+console.log(julien.pays);  // Affiche "France"
+console.log(julien.age);  // Affiche 36
+
+julien.pays = "Italie";
+console.log(julien.pays);  // Affiche maintenant "Italie"
